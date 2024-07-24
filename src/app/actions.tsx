e05411799a7cda16e7a031e6adc8e3db6d4e2732
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-24 02:59:06 -0500

    UIState type

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,6 +1,9 @@
 'use server'
 
 import { createAI } from 'ai/rsc'
+import type { ToolInvocation } from 'ai'
+
+import { ReactNode } from 'react';
 
 export type AIState = Array<{
     id?: number;
@@ -9,7 +12,12 @@ export type AIState = Array<{
     content: string;
 }>
 
-export type UIState = Array<{}>
+export type UIState = Array<{
+    id: number;
+    role: 'user' | 'assistant';
+    display: ReactNode;
+    toolInvocations?: ToolInvocation[];
+}>
 
 export const AI = createAI({
     initialAIState: [],
