3b3a1b3ddfb6077a1d47d7f1fa442ab0dfd94520
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-24 02:37:24 -0500

    env variable setup

--- env.mjs
+++ env.mjs
@@ -1,1 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from 'zod'
+
+export const env = createEnv({
+    server: {
+        GOOGLE_GEMINI_KEY: z.string().min(1),
+    },
+    runtimeEnv: {
+        GOOGLE_GEMINI_KEY: process.env.GOOGLE_GEMINI_KEY,
+    },
+})
