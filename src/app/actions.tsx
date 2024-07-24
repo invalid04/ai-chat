24b33828a877d9aaa6fb7105aecbb432fbd43b78
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-24 02:56:14 -0500

    AIState type

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -2,6 +2,15 @@
 
 import { createAI } from 'ai/rsc'
 
+export type AIState = Array<{
+    id?: number;
+    name?: 'get_info';
+    role?: 'user' | 'assistant' | 'system';
+    content: string;
+}>
+
+export type UIState = Array<{}>
+
 export const AI = createAI({
     initialAIState: [],
     initialUIState: [],
